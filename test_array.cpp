7036505fe891f343693f176899bb4f4e28fa3228
wsw123452 <p3.1415@qq.com>
2013-08-28 23:59:27 +0800

    None

---
+++ test_array.cpp
@@ -0,0 +1,16 @@
+#include <skynet/core/point.hpp>
+//using namespace skynet;
+#include <array>
+#include <thread>
+#include <functional>
+#include <regex>
+#include <iostream>
+
+int main(){
+	std::array<int,3> a;
+//	skynet::point<int,2> p(3,3);	
+
+//	std::cout << p[0] << "," << p[1] << std::endl;
+
+	return 0;
+}
---
+++ test_array.gyp
@@ -0,0 +1,16 @@
+
+{
+"make_global_settings":[
+	["CXX", "/usr/bin/g++"],
+	["LNK", "/usr/bin/g++"],
+],
+"targets":[
+	{
+	"target_name" : "test",
+	"type": "executable",
+	"include_dirs":["./"],
+
+	"sources" : ["test_array.cpp"],
+	},
+]
+}
